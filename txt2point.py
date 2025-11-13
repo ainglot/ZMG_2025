f6c88ac26234ed6e0a7419f883f89ec5ef6615b4
ainglot <ainglot@gmail.com>
2025-11-13 18:33:46 +0100

    Silos w Gdyni

--- txt2point.py
+++ txt2point.py
@@ -4,7 +4,8 @@ import os
 # --- Ustawienia ---
 gdb_path = r"C:\PG\ZMG_2025_26\ArcGIS_ZMG\ArcGIS_ZMG.gdb"
 txt_path = r"C:\PG\ZMG_2025_26\ArcGIS_ZMG\data.txt"
-output_fc_name = "Punkty_z_txt" #54.528821794446145, 18.557495816016086
+output_fc_name = "Punkty_z_txt_v3" #54.528821794446145, 18.557495816016086
+Centr = [471329.7, 740641.8]
 
 arcpy.env.workspace = gdb_path
 # arcpy.env.overwriteOutput = True
@@ -35,6 +36,21 @@ with open(txt_path, 'r') as f:
 if not ListCoor:
     raise ValueError("Nie wczytano żadnych poprawnych współrzędnych.")
 
+# --- Oblicz średnie X i Y ---
+if ListCoor:
+    sum_x = sum(coord[0] for coord in ListCoor)
+    sum_y = sum(coord[1] for coord in ListCoor)
+    n = len(ListCoor)
+
+    sr_x = sum_x / n
+    sr_y = sum_y / n
+
+    print(f"Średnia X: {sr_x:.3f}")
+    print(f"Średnia Y: {sr_y:.3f}")
+    print(f"Punkt środkowy: ({sr_x:.3f}, {sr_y:.3f})")
+else:
+    print("Brak punktów – nie można obliczyć średniej.")
+
 # --- Utwórz nową warstwę punktową ---
 spatial_reference = arcpy.Describe("R2014_OT_ADMS_P").spatialReference  # przejmij układ z istniejącej warstwy
 # Alternatywnie: arcpy.SpatialReference(4326) dla WGS84, jeśli dane są w stopniach
@@ -50,6 +66,7 @@ arcpy.management.CreateFeatureclass(
 # --- Wstaw punkty ---
 with arcpy.da.InsertCursor(output_fc_name, ["SHAPE@X", "SHAPE@Y"]) as cursor:
     for coor in ListCoor:
-        cursor.insertRow(coor)
+        new_coor = [coor[0]-sr_x+Centr[0], coor[1]-sr_y+Centr[1]]
+        cursor.insertRow(new_coor)
 
 print(f"Utworzono warstwę: {output_fc_name} z {len(ListCoor)} punktami.")
