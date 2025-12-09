87c80bfe6cf43dcd40b6966e3a04404a0b05f042
ainglot <ainglot@gmail.com>
2025-12-09 17:49:57 +0100

    Zaczynamy od odczytywania współrzędnych lini z OSM.

--- Rastry_i_Wektory.py
+++ Rastry_i_Wektory.py
@@ -1,4 +1,35 @@
-
-
+import arcpy
 
 ## WFS - siatka NMT: https://mapy.geoportal.gov.pl/wss/service/PZGIK/NumerycznyModelTerenuEVRF2007/WFS/Skorowidze
+
+# Ustawienie geobazy roboczej
+arcpy.env.workspace = r"C:\PG\ZMG_2025_26\ArcGIS_ZMG\ArcGIS_ZMG.gdb"
+# Nazwa warstwy (feature class)
+WarstwaLinie = "DrogaNaRysy_PL92"
+
+# Lista na współrzędne (jeśli chcesz je później wykorzystać)
+ListCoorLinie = []
+cursor = arcpy.da.SearchCursor(WarstwaLinie, ["SHAPE@"])
+
+for row in cursor:
+    geom = row[0]  # obiekt geometryczny linii
+    print(f"--- Obiekt {i} ---")
+    
+    ListCoorOb = []
+    # Iteracja po częściach (część = pojedyncza linia lub segment multipart)
+    for part in geom:
+        print("  Część obiektu:")
+        
+        # Iteracja po wierzchołkach danej części
+        for pnt in part:
+            if pnt:  # niektóre części mogą mieć None
+                print(f"    Wierzchołek: X={pnt.X}, Y={pnt.Y}")
+                ListCoorOb.append((pnt.X+100, pnt.Y+100))  # zapis do listy
+    print(ListCoorOb)
+    ListCoorLinie.append(ListCoorOb)
+    i += 1
+
+print(len(ListCoorLinie))
+
+
+print("KONIEC")
