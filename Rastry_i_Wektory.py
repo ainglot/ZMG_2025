b9bdd4819118a7e3579de5c3d99c59cc4326ac22
ainglot <ainglot@gmail.com>
2025-12-09 17:58:43 +0100

    Lista rastrów

--- Rastry_i_Wektory.py
+++ Rastry_i_Wektory.py
@@ -11,25 +11,37 @@ WarstwaLinie = "DrogaNaRysy_PL92"
 ListCoorLinie = []
 cursor = arcpy.da.SearchCursor(WarstwaLinie, ["SHAPE@"])
 
+i = 0
 for row in cursor:
     geom = row[0]  # obiekt geometryczny linii
-    print(f"--- Obiekt {i} ---")
+    # print(f"--- Obiekt {i} ---")
     
     ListCoorOb = []
     # Iteracja po częściach (część = pojedyncza linia lub segment multipart)
     for part in geom:
-        print("  Część obiektu:")
+        # print("  Część obiektu:")
         
         # Iteracja po wierzchołkach danej części
         for pnt in part:
             if pnt:  # niektóre części mogą mieć None
-                print(f"    Wierzchołek: X={pnt.X}, Y={pnt.Y}")
+                # print(f"    Wierzchołek: X={pnt.X}, Y={pnt.Y}")
                 ListCoorOb.append((pnt.X+100, pnt.Y+100))  # zapis do listy
-    print(ListCoorOb)
+    # print(ListCoorOb)
     ListCoorLinie.append(ListCoorOb)
     i += 1
 
-print(len(ListCoorLinie))
+print(len(ListCoorLinie[0]))
+
+
+##### RASTRY
+Folder_rastry = r"C:\PG\ZMG_2025_26\ArcGIS_ZMG\NMT_TATRY"
+arcpy.env.workspace = Folder_rastry
+
+# Get and print a list of GRIDs from the workspace
+rasters = arcpy.ListRasters("*", "GRID")
+for raster in rasters:
+    print(raster)
+
 
 
 print("KONIEC")
